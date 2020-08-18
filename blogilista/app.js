cbb0b8c2ee49204d533de8ac0edf5941869efc36
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-08-18 22:32:05 +0300

    4.2 blogilista, step 2 - app.js:n eriyttäminen

---
+++ blogilista/app.js
@@ -0,0 +1,20 @@
+// const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+
+require('dotenv').config()
+
+
+const mongoUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fullstack2020-bysw0.mongodb.net/blogilista?retryWrites=true&w=majority`
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- blogilista/index.js
+++ blogilista/index.js
@@ -1,23 +1,9 @@
-// const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http')
+const app = require('./app')
 
-const blogsRouter = require('./controllers/blogs')
-
-require('dotenv').config()
-
-
-const mongoUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fullstack2020-bysw0.mongodb.net/blogilista?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const server = http.createServer(app)
 
 const PORT = 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
